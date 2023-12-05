c32c27c21f7ae79d5a6a3885b56b53ba8efaedec
thilagan001 <133343270+thilagan001@users.noreply.github.com>
2023-12-05 17:22:24 +0530

    club mgmt system

---
+++ about.php
@@ -0,0 +1,9 @@
+<section class="py-3">
+    <div class="container">
+        <h3 class="text-center fw-bolder">About Us</h3>
+        <hr>
+        <div>
+            <?php include "about.html" ?>
+        </div>
+    </div>
+</section>
